4db67b0a9a907ae6bd2795c664b439b4c5fe82ae
tduong992 <tram.anh.duong@g.md-systems.ch>
2015-12-09 10:10:57 +0100

    Replace 2nd test with string_long

--- src/Tests/TweetEmbedFormatterTest.php
+++ src/Tests/TweetEmbedFormatterTest.php
@@ -166,7 +166,7 @@ class TweetEmbedFormatterTest extends WebTestBase {
   }
 
   /**
-   * Tests adding and editing a twitter embed formatter.
+   * Tests adding and editing an embed code type twitter embed formatter.
    */
   public function testManageFieldFormatter() {
     // Test and create one media bundle.
@@ -180,9 +180,9 @@ class TweetEmbedFormatterTest extends WebTestBase {
     // Add and save field settings (Embed code).
     $this->drupalGet('admin/structure/media/manage/' . $bundle->id() . '/fields/add-field');
     $edit_conf = [
-      'new_storage_type' => 'link',
-      'label' => 'Tweet URL',
-      'field_name' => 'tweet_url',
+      'new_storage_type' => 'string_long',
+      'label' => 'Embed code',
+      'field_name' => 'embed_code',
     ];
     $this->drupalPostForm(NULL, $edit_conf, t('Save and continue'));
     $this->assertText('These settings apply to the ' . $edit_conf['label'] . ' field everywhere it is used.');
@@ -196,24 +196,22 @@ class TweetEmbedFormatterTest extends WebTestBase {
     // Set the new field as required.
     $edit = [
       'required' => TRUE,
-      'settings[link_type]' => '16',
-      'settings[title]' => '0',
     ];
     $this->drupalPostForm(NULL, $edit, t('Save settings'));
     $this->assertText('Saved ' . $edit_conf['label'] . ' configuration.');
 
     // Assert that the new field configuration has been successfully saved.
-    $xpath = $this->xpath('//*[@id="field-tweet-url"]');
-    $this->assertEqual((string) $xpath[0]->td[0], 'Tweet URL');
-    $this->assertEqual((string) $xpath[0]->td[1], 'field_tweet_url');
-    $this->assertEqual((string) $xpath[0]->td[2]->a, 'Link');
+    $xpath = $this->xpath('//*[@id="field-embed-code"]');
+    $this->assertEqual((string) $xpath[0]->td[0], 'Embed code');
+    $this->assertEqual((string) $xpath[0]->td[1], 'field_embed_code');
+    $this->assertEqual((string) $xpath[0]->td[2]->a, 'Text (plain, long)');
 
     // Test if edit worked and if new field values have been saved as
     // expected.
     $this->drupalGet('admin/structure/media/manage/' . $bundle->id());
     $this->assertFieldByName('label', $bundle->label());
     $this->assertFieldByName('type', 'twitter');
-    $this->assertFieldByName('type_configuration[twitter][source_field]', 'field_tweet_url');
+    $this->assertFieldByName('type_configuration[twitter][source_field]', 'field_embed_code');
     $this->drupalPostForm(NULL, NULL, t('Save media bundle'));
     $this->assertText('The media bundle ' . $bundle->label() . ' has been updated.');
     $this->assertText($bundle->label());
@@ -222,8 +220,8 @@ class TweetEmbedFormatterTest extends WebTestBase {
 
     // Set and save the settings of the new field.
     $edit = [
-      'fields[field_tweet_url][label]' => 'above',
-      'fields[field_tweet_url][type]' => 'twitter_embed',
+      'fields[field_embed_code][label]' => 'above',
+      'fields[field_embed_code][type]' => 'twitter_embed',
     ];
     $this->drupalPostForm(NULL, $edit, t('Save'));
     $this->assertText('Your settings have been saved.');
@@ -232,17 +230,22 @@ class TweetEmbedFormatterTest extends WebTestBase {
     $this->drupalGet('media/add/' . $bundle->id());
 
     // Random image from twitter.
-    $tweet_url = 'https://twitter.com/RamzyStinson/status/670650348319576064';
+    $tweet_url = '<blockquote class="twitter-tweet" lang="it"><p lang="en" dir="ltr">' .
+                 'Midnight project. I ain&#39;t got no oven. So I improvise making this milo crunchy kek batik. hahahaha ' .
+                 '<a href="https://twitter.com/hashtag/itssomething?src=hash">#itssomething</a> ' .
+                 '<a href="https://t.co/Nvn4Q1v2ae">pic.twitter.com/Nvn4Q1v2ae</a></p>&mdash; Zi (@RamzyStinson) ' .
+                 '<a href="https://twitter.com/RamzyStinson/status/670650348319576064">' .
+                 '28 Novembre 2015</a></blockquote><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>';
 
     $edit = [
       'name[0][value]' => 'Title',
-      'field_tweet_url[0][uri]' => $tweet_url,
+      'field_embed_code[0][value]' => $tweet_url,
     ];
     $this->drupalPostForm(NULL, $edit, t('Save'));
 
     // Assert that the media has been successfully saved.
     $this->assertText('Title');
-    $this->assertText('Tweet URL');
+    $this->assertText('Embed code');
 
     // Assert that the formatter exists on this page.
     $this->assertFieldByXPath('/html/body/div/main/div/div/article/div/div');
