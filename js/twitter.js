ef412841b5666e07b09c1c5d3643597f2767aa84
jaesin <jaesin@841054.no-reply.drupal.org>
2016-02-25 21:32:22 +0530

    Issue #2675244 by Jaesin, Dragan Eror: Simplify Tweet display JS by relying on API provided by Twitter.

--- js/twitter.js
+++ js/twitter.js
@@ -2,25 +2,9 @@
   "use strict";
 
   Drupal.behaviors.twitterMediaEntity = {
-
-    createTweets: function (context) {
-      // this === window
-      $('blockquote.twitter-tweet > a', context).each(function () {
-        // this === link
-        var tweetID = this.href.substr(this.href.lastIndexOf('/') + 1);
-        twttr.widgets.createTweet(tweetID, this.parentNode);
-      });
-    },
-
     attach: function (context) {
-      if (typeof twttr === 'undefined') {
-        $.getScript('//platform.twitter.com/widgets.js', this.createTweets.bind(window, context));
-      }
-      else {
-        this.createTweets(context);
-      }
+      twttr.widgets.load();
     }
-
   };
 
 })(jQuery, Drupal);
