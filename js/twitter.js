40bcd62a74d0129acac83b1ac1562f3d3314a9a3
phenaproxima <phenaproxima@205645.no-reply.drupal.org>
2016-07-11 17:53:23 -0400

    Issue #2764411 by phenaproxima, slashrsm: Fix - Tweets embedded in CKEditor do not load.

--- js/twitter.js
+++ js/twitter.js
@@ -7,7 +7,18 @@
 
   Drupal.behaviors.twitterMediaEntity = {
     attach: function (context) {
-      twttr.widgets.load();
+      function _init () {
+        twttr.widgets.load(context);
+      }
+
+      // If the tweet is being embedded in a CKEditor's iFrame the widgets
+      // library might not have been loaded yet.
+      if (typeof twttr == 'undefined') {
+        $.getScript('//platform.twitter.com/widgets.js', _init);
+      }
+      else {
+        _init();
+      }
     }
   };
 
