ac6b3dcb9cf4e933c1759bcedf9e54f54e4bdaae
Steve Richards <srichards@inviqa.com>
2016-12-06 10:27:02 +0000

    adjusting naming convention to reflect fid mapping

--- src/Plugin/MediaEntity/Type/Twitter.php
+++ src/Plugin/MediaEntity/Type/Twitter.php
@@ -133,7 +133,7 @@ class Twitter extends MediaTypeBase {
         'image' => $this->t('Link to the twitter image'),
         'image_local' => $this->t('Copies tweet image to the local filesystem and returns the URI.'),
         'image_local_uri' => $this->t('Gets URI of the locally saved image.'),
-        'image_field' => $this->t('Map the locally saved object to an image field.'),
+        'image_fid' => $this->t('Map the locally saved object file id to an image field.'),
         'content' => $this->t('This tweet content'),
         'retweet_count' => $this->t('Retweet count for this tweet'),
         'profile_image_url_https' => $this->t('Link to profile image')
@@ -199,7 +199,7 @@ class Twitter extends MediaTypeBase {
           }
           return FALSE;
 
-        case 'image_field':
+        case 'image_fid':
           $local_uri = $this->getField($media, 'image_local');
 
           if ($local_uri && file_exists($local_uri)) {
